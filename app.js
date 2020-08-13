5449634499f7aae6ffbec4fa99aec5b90047bd73
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-08-13 19:50:43 +0700

    update :fire: :fire:

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const ejs = require("ejs");
 const multer = require("multer");
 
@@ -6,8 +7,35 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-     res.render("index");
-}); 
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    var dir = "./uploads";
 
-app.listen(8080);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    callback(null, dir);
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage }).array("files", 12);
+
+app.post("/upload", (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ message: err });
+    }
+    res.status(200).json({ request });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(8080, () => {
+  console.log("connected to port 8080");
+});
